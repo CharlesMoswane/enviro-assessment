6fc347f1fa55d8f97eba9adc342852bb088ac00a
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-08-06 22:41:57 +0200

    Initial commit

---
+++ src/test/java/com/enviro/assessment/grad001/charlesmoswane/enviro_assessment/EnviroAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.charlesmoswane.enviro_assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnviroAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
