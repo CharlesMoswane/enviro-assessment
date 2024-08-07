1727ead02712cbb9bceb7a75a1f75f41989def2f
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-08-07 17:10:00 +0200

    feat: Add EnvironmentalData entity implementation

--- src/main/java/com/enviro/assessment/grad001/charlesmoswane/enviro_assessment/entity/EnvironmentalData.java
+++ src/main/java/com/enviro/assessment/grad001/charlesmoswane/enviro_assessment/entity/EnvironmentalData.java
@@ -1,7 +1,19 @@
 package com.enviro.assessment.grad001.charlesmoswane.enviro_assessment.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class EnvironmentalData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String dataPoint;
+    private String value;
+
 }
