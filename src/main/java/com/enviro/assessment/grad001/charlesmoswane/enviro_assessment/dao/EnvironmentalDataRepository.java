4e5162f0cd75f61991cadc5ebca064b6a018dc06
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-08-07 17:30:00 +0200

    feat: Add environmental data controller

--- src/main/java/com/enviro/assessment/grad001/charlesmoswane/enviro_assessment/dao/EnvironmentalDataRepository.java
+++ src/main/java/com/enviro/assessment/grad001/charlesmoswane/enviro_assessment/dao/EnvironmentalDataRepository.java
@@ -2,6 +2,10 @@ package com.enviro.assessment.grad001.charlesmoswane.enviro_assessment.dao;
 
 import com.enviro.assessment.grad001.charlesmoswane.enviro_assessment.entity.EnvironmentalData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("api/environmental")
 public interface EnvironmentalDataRepository extends JpaRepository<EnvironmentalData, Long> {
 }
