3c119c5a09c9cb40e4ef06cc66badcca512905f7
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-08-12 17:10:00 +0200

    feat: Enable swagger

--- src/main/java/com/enviro/assessment/grad001/charlesmoswane/enviro_assessment/EnviroAssessmentApplication.java
+++ src/main/java/com/enviro/assessment/grad001/charlesmoswane/enviro_assessment/EnviroAssessmentApplication.java
@@ -2,8 +2,10 @@ package com.enviro.assessment.grad001.charlesmoswane.enviro_assessment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class EnviroAssessmentApplication {
 
 	public static void main(String[] args) {
