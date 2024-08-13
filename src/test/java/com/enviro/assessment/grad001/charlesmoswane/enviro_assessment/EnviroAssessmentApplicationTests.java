2ea28f6dcb862cd9ffe656dcc53fbede8d7e5e09
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-08-13 17:20:00 +0200

    feat: Add unit test testGetAllEnvironmentalData()

--- src/test/java/com/enviro/assessment/grad001/charlesmoswane/enviro_assessment/EnviroAssessmentApplicationTests.java
+++ src/test/java/com/enviro/assessment/grad001/charlesmoswane/enviro_assessment/EnviroAssessmentApplicationTests.java
@@ -1,13 +1,25 @@
 package com.enviro.assessment.grad001.charlesmoswane.enviro_assessment;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class EnviroAssessmentApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void testGetAllEnvironmentalData() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/environmentalData"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$._embedded.environmentalData").exists());
 	}
 
 }
