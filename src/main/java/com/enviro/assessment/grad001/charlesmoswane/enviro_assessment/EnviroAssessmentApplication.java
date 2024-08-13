4f2da11c42645347d8b00c8cb45d7e64b0a57a4b
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-08-13 17:10:00 +0200

    feat: Replace springfox with Springdoc OpenAPI

--- src/main/java/com/enviro/assessment/grad001/charlesmoswane/enviro_assessment/EnviroAssessmentApplication.java
+++ src/main/java/com/enviro/assessment/grad001/charlesmoswane/enviro_assessment/EnviroAssessmentApplication.java
@@ -2,7 +2,6 @@ package com.enviro.assessment.grad001.charlesmoswane.enviro_assessment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class EnviroAssessmentApplication {
--- src/main/java/com/enviro/assessment/grad001/charlesmoswane/enviro_assessment/config/AppConfig.java
+++ src/main/java/com/enviro/assessment/grad001/charlesmoswane/enviro_assessment/config/AppConfig.java
@@ -1,4 +1,8 @@
 package com.enviro.assessment.grad001.charlesmoswane.enviro_assessment.config;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
+
 }
