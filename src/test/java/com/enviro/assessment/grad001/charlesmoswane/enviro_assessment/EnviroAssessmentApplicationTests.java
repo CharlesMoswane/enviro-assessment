2b7dd3ccb69fab9f15962b7f8dfe9be8c79ffc72
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-08-13 17:30:00 +0200

    feat: Add test display name

--- src/test/java/com/enviro/assessment/grad001/charlesmoswane/enviro_assessment/EnviroAssessmentApplicationTests.java
+++ src/test/java/com/enviro/assessment/grad001/charlesmoswane/enviro_assessment/EnviroAssessmentApplicationTests.java
@@ -1,7 +1,9 @@
 package com.enviro.assessment.grad001.charlesmoswane.enviro_assessment;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -10,16 +12,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class EnviroAssessmentApplicationTests {
 
 	@Autowired
 	private MockMvc mockMvc;
 
 	@Test
-	public void testGetAllEnvironmentalData() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/api/environmentalData"))
-				.andExpect(status().isOk())
-				.andExpect(jsonPath("$._embedded.environmentalData").exists());
+	@DisplayName("Test get environmental data before file upload")
+	public void testGetAllEnvironmentalDataEmpty() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/environmental/data"))
+				.andExpect(status().isOk());
 	}
-
 }
